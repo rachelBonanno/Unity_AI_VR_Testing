ef2bad805047424618d17efb91a26d330dd83609
rachelBonanno <rachel.bonanno@tufts.edu>
2022-11-19 15:29:12 -0500

    adding multiple and error due to ; in FlockUnit

---
+++ flockingAIfishandUnitycompatibility/Assets/Scripts/Flock.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+   [Header("Spawn Setup")]
+   [SerializeField] private FlockUnit flockUnityPrefab;
+   [SerializeField] private int flocksize;
+   [SerializeField] private Vector3 spawnBounds;
+
+   [Header("Speed Setup")] 
+   [Range(0,10)]
+   [SerializeField] private float minSpeed;
+   [Range(0,10)]
+   [SerializeField] private float maxSpeed;
+
+
+   [Header("Detection Distances")] 
+   [Range(0,10)]
+   [SerializeField] private float _cohesionDistance;
+   public float cohesionDistance
+   {
+      get { return _cohesionDistance; }
+   }
+   
+   public FlockUnit[] allUnits { get; set; }
+
+   private void Start()
+   {
+      GenerateUnits();
+   }
+
+   private void Update()
+   {
+      for (int i = 0; i < allUnits.Length; i++)
+      {
+         allUnits[i].MoveUnit();
+      }
+   }
+
+   private void GenerateUnits()
+   {
+      allUnits = new FlockUnit[flocksize];
+      for (int i = 0; i < flocksize; i++)
+      {
+         var randomVector = UnityEngine.Random.insideUnitSphere;
+         randomVector = new Vector3(randomVector.x * spawnBounds.x, randomVector.y * spawnBounds.y,
+            randomVector.z * spawnBounds.z);
+         var spawnPosition = transform.position + randomVector;
+         var rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0);
+         allUnits[i] = Instantiate(flockUnityPrefab, spawnPosition, rotation);
+         allUnits[i].AssignFlock(this);
+         allUnits[i].IntializeSpeed(UnityEngine.Random.Range(minSpeed, maxSpeed));
+      }
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ flockingAIfishandUnitycompatibility/Assets/Scripts/FlockUnit.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockUnit : MonoBehaviour
+{
+    [SerializeField] private float FOVAngle;
+    [SerializeField] private smoothDamp;
+    private List<FlockUnit> cohesionNeighbours = new List<FlockUnit>();
+    private Flock assignedFlock;
+    private Vector3 currentVelocity;
+    private float speed;
+    public Transform MyTransform { get; set; }
+
+    public void Awake()
+    {
+        myTransform = myTransform;
+    }
+    public void AssignFlock(Flock flock)
+    {
+        assignedFlock = flock;
+    }
+
+    public void InitializeSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+    public void MoveUnit()
+    {
+        FindNeighbours();
+        var cohesionVector = CalculateCohesionVector();
+        var moveVector = Vector3.SmoothDamp(myTransform.forward, cohesionVector, ref currentVelocity, smoothDamp);
+        moveVector = moveVector.normalized * speed;
+        myTransform.forward = moveVector;
+        myTransform.position += moveVector * Time.deltaTime;
+    }
+
+    private FindNeighbours()
+    {
+        cohesionNeighbours.Clear();
+        var allUnits = assignedFlock.allUnits;
+        for (int i = 0; i < allUnits.Length; i++)
+        {
+            var currentUnit = allUnits[i];
+            if (currentUnit != this)
+            {
+                float currentNeighbourDistanceSqr =
+                    Vector3.SqrMagnitude(currentUnit.transform.position - transform.position);
+                if (currentNeighbourDistanceSqr <= assignedFlock.cohesionDistance * assignedFlock.cohesionDistance)
+                {
+                    cohesionNeighbours.Add(currentUnit);
+                }
+            }
+        }
+    }
+
+    private Vector3 CalculateCohesionVector()
+    {
+        var cohesionVector = Vector3.zero;
+        if (cohesionNeighbours.Count == 0)
+        {
+            return cohesionVector;
+        }
+        int neighboursInFOV = 0;
+        for (int i = 0; i < cohesionNeighbours.Count; i++)
+        {
+            if (IsInFOV(cohesionNeighbours[i].myTransform.position))
+            {
+                neighboursInFOV++;
+                cohesionVector += cohesionNeighbours[i].myTransform.position;
+            }
+        }
+
+        if (neighboursInFOV == 0)
+        {
+            return cohesionVector;
+        }
+
+        cohesionVector /= neighboursInFOV;
+        cohesionVector -= myTransform.position;
+        cohesionVector = Vector3.Normalize(cohesionVector);
+        return cohesionVector;
+    }
+
+    private bool IsInFOV(Vector3 position)
+    {
+        return Vector3.Angle(myTransform.forward, position - myTransform.position) <= FOVAngle;
+    }
+}
