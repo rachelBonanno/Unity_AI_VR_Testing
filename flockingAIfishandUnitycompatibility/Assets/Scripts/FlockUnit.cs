e32084571b80fa84468320487eed962fc37dd1a9
rachelBonanno <rachel.bonanno@tufts.edu>
2022-12-19 15:52:56 -0500

    fish move

--- flockingAIfishandUnitycompatibility/Assets/Scripts/FlockUnit.cs
+++ flockingAIfishandUnitycompatibility/Assets/Scripts/FlockUnit.cs
@@ -3,82 +3,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlockUnit : MonoBehaviour {
-    [SerializeField] private float FOVAngle;
-    [SerializeField] private float smoothDamp;
+public class FlockUnit : MonoBehaviour
+{
+	[SerializeField] private float FOVAngle;
+	[SerializeField] private float smoothDamp;
 	[SerializeField] private LayerMask obstacleMask;
 	[SerializeField] private Vector3[] directionsToCheckWhenAvoidingObstacles;
 
-    private List<FlockUnit> cohesionNeighbours = new List<FlockUnit>();
+	private List<FlockUnit> cohesionNeighbours = new List<FlockUnit>();
 	private List<FlockUnit> avoidanceNeighbours = new List<FlockUnit>();
 	private List<FlockUnit> aligementNeighbours = new List<FlockUnit>();
-    private Flock assignedFlock;
-    private Vector3 currentVelocity;
+	private Flock assignedFlock;
+	private Vector3 currentVelocity;
 	private Vector3 currentObstacleAvoidanceVector;
-    private float speed;
+	private float speed;
 
-    public Transform myTransform { get; set; }
+	public Transform myTransform { get; set; }
 
-    public void Awake() {
-        myTransform = myTransform;
-    }
+	private void Awake()
+	{
+		myTransform = transform;
+	}
 
-    public void AssignFlock(Flock flock) {
-        assignedFlock = flock;
-    }
+	public void AssignFlock(Flock flock)
+	{
+		assignedFlock = flock;
+	}
 
-    public void InitializeSpeed(float speed) {
-        this.speed = speed;
-    }
+	public void InitializeSpeed(float speed)
+	{
+		this.speed = speed;
+	}
 
-    public void MoveUnit() {
-        FindNeighbours();
+	public void MoveUnit()
+	{
+		FindNeighbours();
 		CalculateSpeed();
 
-        var cohesionVector = CalculateCohesionVector();
+		var cohesionVector = CalculateCohesionVector() * assignedFlock.cohesionWeight;
 		var avoidanceVector = CalculateAvoidanceVector() * assignedFlock.avoidanceWeight;
 		var aligementVector = CalculateAligementVector() * assignedFlock.aligementWeight;
 		var boundsVector = CalculateBoundsVector() * assignedFlock.boundsWeight;
 		var obstacleVector = CalculateObstacleVector() * assignedFlock.obstacleWeight;
 
-        var moveVector = cohesionVector + avoidanceVector + aligementVector + boundsVector + obstacleVector;
+		var moveVector = cohesionVector + avoidanceVector + aligementVector + boundsVector + obstacleVector;
 		moveVector = Vector3.SmoothDamp(myTransform.forward, moveVector, ref currentVelocity, smoothDamp);
-        moveVector = moveVector.normalized * speed;
-		if (moveVector == Vector3.zero) {
+		moveVector = moveVector.normalized * speed;
+		if (moveVector == Vector3.zero)
 			moveVector = transform.forward;
-		}
-        myTransform.forward = moveVector;
-        myTransform.position += moveVector * Time.deltaTime;
-    }
 
-    private void FindNeighbours() {
-        cohesionNeighbours.Clear();
+		myTransform.forward = moveVector;
+		myTransform.position += moveVector * Time.deltaTime;
+	}
+
+	
+
+	private void FindNeighbours()
+	{
+		cohesionNeighbours.Clear();
 		avoidanceNeighbours.Clear();
 		aligementNeighbours.Clear();
-        var allUnits = assignedFlock.allUnits;
-        for (int i = 0; i < allUnits.Length; i++) {
-            var currentUnit = allUnits[i];
-            if (currentUnit != this) {
-                float currentNeighbourDistanceSqr = Vector3.SqrMagnitude(currentUnit.myTransform.position - myTransform.position);
-                if (currentNeighbourDistanceSqr <= assignedFlock.cohesionDistance * assignedFlock.cohesionDistance) {
-                    cohesionNeighbours.Add(currentUnit);
-                }
-				if (currentNeighbourDistanceSqr <= assignedFlock.avoidanceDistance * assignedFlock.avoidanceDistance) {
+		var allUnits = assignedFlock.allUnits;
+		for (int i = 0; i < allUnits.Length; i++)
+		{
+			var currentUnit = allUnits[i];
+			if (currentUnit != this)
+			{
+				float currentNeighbourDistanceSqr = Vector3.SqrMagnitude(currentUnit.myTransform.position - myTransform.position);
+				if(currentNeighbourDistanceSqr <= assignedFlock.cohesionDistance * assignedFlock.cohesionDistance)
+				{
+					cohesionNeighbours.Add(currentUnit);
+				}
+				if (currentNeighbourDistanceSqr <= assignedFlock.avoidanceDistance * assignedFlock.avoidanceDistance)
+				{
 					avoidanceNeighbours.Add(currentUnit);
 				}
-				if (currentNeighbourDistanceSqr <= assignedFlock.aligementDistance * assignedFlock.aligementDistance) {
+				if (currentNeighbourDistanceSqr <= assignedFlock.aligementDistance * assignedFlock.aligementDistance)
+				{
 					aligementNeighbours.Add(currentUnit);
 				}
-            }
-        }
-    }
-
-	private void CalculateSpeed() {
-		if (cohesionNeighbours.Count == 0) {
-			return;
+			}
 		}
+	}
+
+	private void CalculateSpeed()
+	{
+		if (cohesionNeighbours.Count == 0)
+			return;
 		speed = 0;
-		for (int i = 0; i < cohesionNeighbours.Count; i++) {
+		for (int i = 0; i < cohesionNeighbours.Count; i++)
+		{
 			speed += cohesionNeighbours[i].speed;
 		}
 
@@ -86,26 +100,28 @@ public class FlockUnit : MonoBehaviour {
 		speed = Mathf.Clamp(speed, assignedFlock.minSpeed, assignedFlock.maxSpeed);
 	}
 
-    private Vector3 CalculateCohesionVector() {
-        var cohesionVector = Vector3.zero;
-        if (cohesionNeighbours.Count == 0) {
-            return Vector3.zero;
-        }
-        int neighboursInFOV = 0;
-        for (int i = 0; i < cohesionNeighbours.Count; i++) {
-            if (IsInFOV(cohesionNeighbours[i].myTransform.position)) {
-                neighboursInFOV++;
-                cohesionVector += cohesionNeighbours[i].myTransform.position;
-            }
-        }
+	private Vector3 CalculateCohesionVector()
+	{
+		var cohesionVector = Vector3.zero;
+		if (cohesionNeighbours.Count == 0)
+			return Vector3.zero;
+		int neighboursInFOV = 0;
+		for (int i = 0; i < cohesionNeighbours.Count; i++)
+		{
+			if (IsInFOV(cohesionNeighbours[i].myTransform.position))
+			{
+				neighboursInFOV++;
+				cohesionVector += cohesionNeighbours[i].myTransform.position;
+			}
+		}
 
-        cohesionVector /= neighboursInFOV;
-        cohesionVector -= myTransform.position;
-        cohesionVector = cohesionVector.normalized;
-        return cohesionVector;
-    }
+		cohesionVector /= neighboursInFOV;
+		cohesionVector -= myTransform.position;
+		cohesionVector = cohesionVector.normalized;
+		return cohesionVector;
+	}
 
-private Vector3 CalculateAligementVector()
+	private Vector3 CalculateAligementVector()
 	{
 		var aligementVector = myTransform.forward;
 		if (aligementNeighbours.Count == 0)
@@ -204,7 +220,8 @@ private Vector3 CalculateAligementVector()
 		return selectedDirection.normalized;
 	}
 
-    private bool IsInFOV(Vector3 position) {
-        return Vector3.Angle(myTransform.forward, position - myTransform.position) <= FOVAngle;
-    }
+	private bool IsInFOV(Vector3 position)
+	{
+		return Vector3.Angle(myTransform.forward, position - myTransform.position) <= FOVAngle;
+	}
 }
